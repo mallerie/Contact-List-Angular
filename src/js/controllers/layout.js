36169d2c72d18af6d9771d03396c43d3d272b0f6
Mallerie Shirley <mallerie@gmail.com>
2016-11-21 14:58:12 -0500

    Added route for root state and set up layout controller

---
+++ src/js/controllers/layout.js
@@ -0,0 +1,10 @@
+function  LayoutController () {
+  console.log("Layout Controller ran succesfully");
+
+  let vm = this;
+
+  //Any methods should go here.
+}
+
+LayoutController.$inject = [];
+export { LayoutController };
---
+++ src/js/routes.js
@@ -0,0 +1,8 @@
+function routerConfig ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('root', {
+      abstract: true,
+      templateUrl: 'templates/layout.tpl.html',
+      controller: 'LayoutController as layout'
+    })
+}
